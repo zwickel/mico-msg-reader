f20e4cb47424a80a7f27f087b0a1a4d7b68abd12
ma <mariusaltmann@outlook.de>
2021-03-11 11:12:21 +0100

    adding project src files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mico-msg-reader
+Provides an example Service which reads messages from a Kafka topic and provides them via HTTP for user inspection
+
+# Webview
+
+Provides a message view at http://localhost:8090/messageViewer.html
---
+++ src/main/java/io/github/ust/mico/msgvalidator/MessageListener.java
@@ -0,0 +1,30 @@
+package io.github.ust.mico.msgvalidator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+@Slf4j
+@Component
+public class MessageListener {
+
+
+    /**
+     * Used for messaging via stomp and websockets
+     */
+    @Autowired
+    SimpMessagingTemplate websocketsTemplate;
+
+    /**
+     * Entry point for incoming messages from kafka.
+     *
+     * @param message
+     */
+    @KafkaListener(topics = "${kafka.input-topic}", groupId = "${kafka.group-id}")
+    public void receive(String message) {
+        log.debug("Received CloudEvent message: {}", message);
+        websocketsTemplate.convertAndSend("/topic/messaging-bridge", message);
+    }
+
+}
